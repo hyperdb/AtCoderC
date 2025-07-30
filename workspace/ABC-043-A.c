69a32de61f2447ac5f42ab6046cedad3a6513375
hyperdb <hyperdb@gmail.com>
2025-07-30 18:01:41 +0900

    ✨解答を追加 - ABC-043-A

---
+++ workspace/ABC-043-A.c
@@ -0,0 +1,19 @@
+/**
+ * ABC-043-A
+ */
+#include <stdio.h>
+
+/**
+ *
+ */
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int k = (1 + n) * n / 2;
+
+    printf("%d\n", k);
+
+    return 0;
+}
